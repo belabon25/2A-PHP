3d3ff9d2e7885b16a3da3a794c5ca37949b35d4f
bonjour <a@bbb.c>
2021-11-22 21:13:49 +0100

    Ajout de bootstrap + tests mise en place

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<link rel="stylesheet" href="config/bootstrap-5.1.3-dist/css/bootstrap.min.css">
+<meta charset="utf-8">
+</head>
 <?php
 require("config/utils.php");
 
--- vues/vueTaskPublic.php
+++ vues/vueTaskPublic.php
@@ -1,14 +1,46 @@
-<?php
-$gatewayTask=new gatewayTask($dsn,$user,$passwd);
-$res=$gatewayTask->getTask();
+<!DOCTYPE html>
+<html lang="en">
 
-foreach($res as $task){
-    echo $task->getName()."<br>";
-    /*echo $task->getDescription()."<br>";
+<head>
+<link rel="stylesheet" href="../config/bootstrap-5.1.3-dist/css/bootstrap.min.css">
+<meta charset="utf-8">
+</head>
+<body>
+<div class="alert alert-primary" role="alert">
+  A simple primary alert—check it out!
+</div>
+<div class="jumbotron text-center">
+        <h1>My First Bootstrap Page</h1>
+        <p>Resize this responsive page to see the effect!</p>
+    </div>
+    <div class="container">
+        <div class="row">
+ 
+        </div>
+        <div class="col-sm-4">
+            <h3>Column 2</h3>
+            <p>Lorem ipsum dolor..</p>
+        </div>
+        <div class="col-sm-4">
+            <h3>Column 3</h3>
+            <p>Lorem ipsum dolor..</p>
+        </div>
+    </div>
+    </div>
+</body>
+</html>
+
+<?php
+            $gatewayTask = new gatewayTask($dsn, $user, $passwd);
+            $res = $gatewayTask->getTask();
+
+            foreach ($res as $task) {
+                echo $task->getName() . "<br>";
+                /*echo $task->getDescription()."<br>";
     echo $task->getDateDeb()."<br>";
     echo $task->getDateFin()."<br>";*/
-}
+            }
 
-$gwtd = new gatewayTodolist($dsn,$user,$passwd);
+            $gwtd = new gatewayTodolist($dsn, $user, $passwd);
 
-$gwtd->getAllPublicLists();
+            $gwtd->getAllPublicLists(); ?>
