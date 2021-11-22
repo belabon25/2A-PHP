f3c2a4be15919340f4bca89c3bbd472d68dab961
yogourves <yoann.gourves@gmail.com>
2021-11-22 18:41:32 +0100

    new db

--- db.sql
+++ db.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6deb4+deb9u1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Client :  localhost:3306
--- Généré le :  Ven 19 Novembre 2021 à 11:20
--- Version du serveur :  10.3.31-MariaDB-0+deb10u1
--- Version de PHP :  7.3.31-1~deb10u1
+-- Hôte : 127.0.0.1
+-- Généré le : lun. 22 nov. 2021 à 18:39
+-- Version du serveur : 10.4.21-MariaDB
+-- Version de PHP : 8.0.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,53 +18,49 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `dbyogourves`
+-- Base de données : `dbyogourves`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Tache`
+-- Structure de la table `user`
 --
 
-CREATE TABLE `Tache` (
+CREATE TABLE `user` (
   `id` int(11) NOT NULL,
-  `name` varchar(20) NOT NULL,
-  `description` varchar(500) NOT NULL,
-  `isPrivate` tinyint(1) NOT NULL DEFAULT 0,
-  `isFinished` tinyint(1) NOT NULL DEFAULT 0,
-  `date` date NOT NULL
+  `name` varchar(30) NOT NULL,
+  `hashPasswd` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Contenu de la table `Tache`
+-- Déchargement des données de la table `user`
 --
 
-INSERT INTO `Tache` (`id`, `name`, `description`, `isPrivate`, `isFinished`, `date`) VALUES
-(1, 'test1', 'fdqdfqdf', 0, 0, '2021-11-19'),
-(2, 'test1', 'fdqdfqdf', 0, 0, '2021-11-19'),
-(3, 'test2', 'qsdfqgfdgsdf', 0, 0, '2021-11-20'),
-(4, 'test2', 'qsdfqgfdgsdf', 0, 0, '2021-11-20');
+INSERT INTO `user` (`id`, `name`, `hashPasswd`) VALUES
+(1, 'test', 'azerty');
 
 --
--- Index pour les tables exportées
+-- Index pour les tables déchargées
 --
 
 --
--- Index pour la table `Tache`
+-- Index pour la table `user`
 --
-ALTER TABLE `Tache`
+ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT pour les tables exportées
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT pour la table `Tache`
+-- AUTO_INCREMENT pour la table `user`
 --
-ALTER TABLE `Tache`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
