fde05a4b1ce0ee87afe918a18a1d86cf2561cd82
yogourves <yoann.gourves@gmail.com>
2021-11-25 18:25:12 +0100

    remove require unecessary

--- vues/vueTaskPrivee.php
+++ vues/vueTaskPrivee.php
@@ -1,5 +1,4 @@
 <?php
-require($GLOBALS["vues"]["vueEnTete"]);
 echo "<div class=\"jumbotron text-center\">
         <div class=\"p-3 mb-2 bg-success text-black\">
             <h2>Listes privées :</h2>
